600bee93a4f9cc4c6ee6dea7931db70abe0e2d62
Hanu Shashwat <hanushashwat733@gmail.com>
2026-03-02 16:44:44 +0530

    Built the logic for Data Layer - AuthRepository Implementation (For Sign Up).

---
+++ lib/auth/data/repositories/auth_repo_impl.dart
@@ -0,0 +1,38 @@
+import 'package:blog_app/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/auth/domain/auth_repository.dart';
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:blog_app/core/error/failures.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepoImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+
+  const AuthRepoImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<Failures, String>> logInWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement logInWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failures, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userID = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return right(userID);
+    } on ServerException catch (e) {
+      return left(Failures(e.message));
+    }
+  }
+}
